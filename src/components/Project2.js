18ea5f48d269fbdf3fb62acb8e05e49734108575
abhinav anshul <abhinavanshul4u@gmail.com>
2019-12-01 21:13:38 +0530

    added project1 image and details

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -14,15 +14,18 @@ class Project2 extends React.Component {
           </div>
           <div className="project-text-container">
             <div className="actual_text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
+              This very application you're on right now. I built it from scratch
+              using ReactJS. I wanted to try the new CSS features, thus I
+              integrated everything I could to make it more modern looking and
+              very minimal. I have used SASS which is the CSS Pre-processor
+              along with the modern CSS tools using CSS Grid and CSS Flexbox to
+              positon various. I have also scaled down this application to a
+              various range of mobile devices so that it can look great on
+              smaller devices too.
             </div>
-            <div className="actual_text-stack">HTML | CSS3 | JAVASCRIPT</div>
+            <div className="actual_text-stack">
+              REACTJS | CSS-FLEXBOX | CSS-GRID | SASS
+            </div>
             <div className="button_container">
               <a target="_blank" href="https://abhinavanshul.com/">
                 <button>Visit</button>
