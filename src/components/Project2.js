ca0ed3329ed463d58f8b1f6c6748a5c2c4f91761
abhinav anshul <abhinavanshul4u@gmail.com>
2019-12-02 00:07:35 +0530

    minor change

--- src/components/Project2.js
+++ src/components/Project2.js
@@ -40,28 +40,36 @@ class Project2 extends React.Component {
           </div>
         </div>
 
-        <div className="project-heading ">Portofolio</div>
+        <div className="project-heading ">Rick & Morty Databse</div>
         <div className="project-image-text-container abc">
           <div className="project-text-container">
             <div className="actual_text">
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-              enim ad minim veniam, quis nostrud exercitation ullamco laboris
-              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
-              reprehenderit in voluptate velit esse cillum dolore eu fugiat
-              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-              sunt in culpa qui officia deserunt mollit anim id est laborum
+              I have created this Rick & Morty Database application which is
+              based on a really popular TV show which goes by the same name. In
+              this application, I am rendering over more than 490 different
+              characters which also differs in various stats. I have used REST
+              API to fetch all the stats including the images from
+              rickandmortyapi.com In addition, I wanted to play with the new
+              Context-API which has been introduced in React v.16 for the global
+              state management, so I built this toggle switch on the top of this
+              application, which can toggle the theme between light/dark
             </div>
-            <div className="actual_text-stack">HTML | CSS3 | JAVASCRIPT</div>
+            <div className="actual_text-stack">
+              REACTJS | CONTEXT-API | REST API
+            </div>
             <div className="button_container">
-              <button>Visit</button>
-              <button>Source</button>
+              <a target="_blank" href="rickandmorty.abhinavanshul.com">
+                <button>Visit</button>
+              </a>
+              <a target="_blank" href="rickandmorty.abhinavanshul.com">
+                <button>Source</button>
+              </a>
             </div>
           </div>
           <div className="project-image-container">
             <img
               className="project1-image"
-              src={require("../assets/project1.PNG")}
+              src={require("../assets/project2.PNG")}
             />
           </div>
         </div>
