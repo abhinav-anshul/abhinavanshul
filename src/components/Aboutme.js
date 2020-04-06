5be88ff2820e3014c7d32616e517d33950def061
abhinav anshul <abhinavanshul4u@gmail.com>
2020-04-06 19:22:01 +0530

    updated intro

--- src/components/Aboutme.js
+++ src/components/Aboutme.js
@@ -6,20 +6,15 @@ class Aboutme extends React.Component {
     return (
       <div className="aboutme_container">
         <div className="aboutme-firstpara">
-          Hey!! I'm <span>Abhinav,</span> a 22 years old Front-end developer,
+          Hey!! I'm <span>Abhinav,</span> a 22 years old FrontEnd Engineer,
           based in India. I graduated in <span>Computer Science</span> from
           <span> Kalinga Institute of Industrial Technology,</span> Bhubaneswar.
         </div>
         <div className="aboutme-secondpara">
-          Immediately after the end of my 7th semester I secured a campus job
-          offer in a well-known company called <span>"Accenture"</span> where I
-          worked for around 6 months, while the workplace was great I was not
-          learning anything new, I was very much hesitant to work on support and
-          consulting projects and I felt stuck. Thus I decided to resign the
-          company in November'19 to search for new interesting roles and
-          opportunities related to <span>Frontend and JavaScript</span> in
-          particular which I <span>love</span> coding in, and creating new
-          things.
+          I have been primarily working with JavaScript since I graduated, and
+          picked up ReactJS for most of my work and personal projects. You can
+          also find me writing Tech articles, engaging with Frontend
+          communities, sharing, and developing ideas!
         </div>
         <div className="aboutme-thirdpara">
           This personal web application is all about me and the{" "}
